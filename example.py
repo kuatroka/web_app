1c588334f673958ccd2b4087f7b745f7a642d5a2
kuatroka <aaarmani_2000@yahoo.com>
2017-06-18 18:03:36 +0100

    .

--- example.py
+++ example.py
@@ -4,18 +4,30 @@ import requests
 app = Flask(__name__)
 
 
+
 @app.route("/")
 def index():
-    return "<h1>Hello Me!</h1>"
+    return render_template('index.html')
 
+@app.route('/example')
+def example():
+    return render_template('example.html')
+
+@app.route('/sign')
+def sign():
+    return render_template('sign.html')
 
 @app.route("/home", methods=['GET', 'POST'])
 def home():
     url = 'https://min-api.cryptocompare.com/data/histominute?fsym=BTC&tsym=USD&limit=\
     10&aggregate=0&e=Kraken'
     url_output = requests.get(url).json()['Data']
-    links = ['https://www.google.com', 'https://www.duckduckgo.com', 'https://www.github.com']
+    links = ['https://www.google.com', 'https://www.duckduckgo.com', \
+    'https://www.github.com']
+
     return render_template('example.html', links=links)
+
+
 
 
 if __name__ == "__main__":
